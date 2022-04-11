34d51800d9a64bf240e89f8740d2e156039f083f
Documenter.jl <documenter@juliadocs.github.io>
2022-04-11 13:34:09 +0000

    build based on 558027e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Dipole.jl","page":"Dipole.jl","title":"Dipole.jl","text":"","category":"section"},{"location":"","page":"Dipole.jl","title":"Dipole.jl","text":"Documentation for Dipole.jl","category":"page"}]
+}
